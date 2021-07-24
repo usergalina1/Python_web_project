8a976b3a8f11379cb8615bfcd2c03d990655a63a
Galina <usergalina1@gmail.com>
2021-07-24 17:45:30 -0500

    Day 5 added the second solution for HW4

--- tests/hw4_cristmas_tree.py
+++ tests/hw4_cristmas_tree.py
@@ -35,6 +35,17 @@ def make_number_tree(height_of_tree: int = random.randint(2, 12)):
 make_number_tree()
 
 
+def make_number_tree_nm(height_of_tree: int = random.randint(2, 12)):
+    for branch in range(height_of_tree):
+        symbol = branch + 1
+        if symbol >= 10:
+            symbol = symbol - 10 * int(symbol/10)
+        print(' ' * (height_of_tree - branch) + str(symbol) * (1 + (branch * 2)))
+    print(' ' * height_of_tree + '|')
+
+make_number_tree_nm(12)
+
+
 #mine
 def make_tree(height_of_tree: int, number: int):
     for branch in range(height_of_tree):
