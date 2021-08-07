05871dab2143b7aa39e310186c52e79025ac55f2
Galina <usergalina1@gmail.com>
2021-08-07 15:05:20 -0500

    Day 9 added HW9 Part1(parameterized template)

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ BROWSER = "chrome"
 CHROME_PATH = ChromeDriverManager().install()
 
 DOMAIN = "http://hrm-online.portnov.com"
+DOMAIN1 = "https://www.calculatorsoup.com/calculators/math/basic.php"
 
 ADMIN_USER = "admin"
 DEFAULT_PASSWORD = "password"
