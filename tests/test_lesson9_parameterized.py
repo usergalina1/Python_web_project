fbc35779cfce51aa3e992b71f42c89a972fab98c
usergalina1 <50974306+usergalina1@users.noreply.github.com>
2021-08-07 15:06:16 -0500

    Day 9 (#12)
    * Day 9 added test parameterized negative TC, optimized HW7
    
    * Day 9 added HW9 Part1(parameterized template)

---
+++ tests/test_lesson9_parameterized.py
@@ -0,0 +1,74 @@
+import unittest
+from time import sleep
+
+from parameterized import parameterized
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from steps.common import authenticate
+from tests import CHROME_PATH, DOMAIN, ADMIN_USER, DEFAULT_PASSWORD
+
+
+class LoginPageTests(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path=CHROME_PATH)
+        self.browser.get(DOMAIN)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    @unittest.skip
+    def test_valid_login(self):
+        original_url = self.browser.current_url
+        self.browser.find_element(By.ID, "txtUsername").send_keys(ADMIN_USER)
+        self.browser.find_element(By.ID, "txtPassword").send_keys(DEFAULT_PASSWORD)
+        self.browser.find_element(By.ID, "btnLogin").click()
+
+        wait = WebDriverWait(self.browser, 7)
+        wait.until(expected_conditions.url_contains("pim/viewEmployeeList"))
+
+        new_url = self.browser.current_url
+        self.assertNotEqual(original_url, new_url)
+        # self.assertIn("pim/viewEmployeeList", new_url)
+
+        welcome_message = self.browser.find_element(By.ID, "welcome").text
+        self.assertEqual("Welcome Admin", welcome_message)
+        self.assertEqual("Employee Information",
+                         self.browser.find_element(By.TAG_NAME, "h1").text)
+
+    @parameterized.expand([
+        ('invalid password', 'admin', '123abc', 'Invalid credentials'),
+        ('empty username', '', '123abc', 'Username cannot be empty'),
+        ('empty password', 'admin', '', 'Password cannot be empty')
+    ])
+    def test_invalid_credentials(self, test_name, username, password, expected_error_message):
+        authenticate(self.browser, username=username, password=password)
+        message = self.browser.find_elements(By.ID, 'spanMessage')
+        self.assertTrue(message)
+        self.assertEqual(expected_error_message, message[0].text)
+
+    # def test_empty_username(self):
+    #     expected_error_message = "Username cannot be empty"
+    #     username = ''
+    #     password = 'password'
+    #
+    #     authenticate(self.browser, username=username, password=password)
+    #     message = self.browser.find_elements(By.ID, 'spanMessage')
+    #     self.assertTrue(message)
+    #     self.assertEqual(expected_error_message, message[0].text)
+
+    # def test_empty_password(self):
+    #     expected_error_message = "Password cannot be empty"
+    #     username = 'admin'
+    #     password = ''
+    #
+    #     authenticate(self.browser, username=username, password=password)
+    #     message = self.browser.find_elements(By.ID, 'spanMessage')
+    #     self.assertTrue(message)
+    #     self.assertEqual(expected_error_message, message[0].text)
+
+
+if __name__ == '__main__':
+    unittest.main()
